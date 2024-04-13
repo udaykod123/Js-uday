bc1a72525471cd1d55ca4d02fd207eb8da35faa4
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-04-13 06:13:45 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code of newer version
+// alert(3+3) we are using node js not browser
+console.log(3+3)
+// code readability should be high
+
+console.log("uday")
+let name = "uday"
+let age = 25
+let isLoggedIn = false
+let undefined;
+let state;
+
+/* premetive Datatypes
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+object
+*/
+console.log(typeof undefined);
+console.log(typeof null)
+
