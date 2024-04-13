a199b3c648b1f7e8084e96906840b38bb103fbea
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-04-13 05:21:42 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 20303980292
+let accountEmail = "uday@kodnest.com"
+ accountPassword = "12345"
+ accountCity = "Bangalore"
+ let accountState;
+// console.log(accountId);
+accountEmail = "mishrauday138@gmail.com"
+accountPassword = "Uday@123"
+accountCity = "Mumbai"
+console.table([accountEmail, accountCity, accountPassword, accountState]);
