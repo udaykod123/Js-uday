29d0b93dcbf53c044a033dff3d9fd6c4e36bff95
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-04-13 20:01:45 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+
+// mostly avoid the these type conversion
+console.log(null >  0);
+console.log(null ==  0);
+console.log(null >= 0);
+
+
+console.log(undefined >  0);
+console.log(undefined ==  0);
+console.log(undefined < 0);
