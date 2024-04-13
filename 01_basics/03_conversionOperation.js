a29262e563038308ab88a91bfe91d370fb4e3505
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-04-13 18:59:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let age = "26"
+console.log(typeof age)
+let valueInNumber = Number(age);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "uday"
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+/* 1 => true; 0 => false;
+"" => false;
+"uday" => true;
+*/
+
+let someNumber = 20
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
