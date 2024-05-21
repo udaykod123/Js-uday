fd1e22292dfe8507a8f4a972f1635da396d1460a
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-05-21 05:47:03 +0000

    scope.js

---
+++ 03_basics/scope.js
@@ -0,0 +1,77 @@
+// // let a = 10;
+// // const b = 20;
+// // var c = 30;
+
+// if(true)
+//     {
+//         let a = 10;
+//         const b = 20;
+//         var c = 30;
+//     }
+// //console.log(a);
+// //console.log(b);
+// console.log(c);
+
+//-------------------------------------------------------------------------->>
+let a = 300
+if(true)
+    {
+        let a = 10
+        const b = 20
+        console.log("INNER :", a);
+    }
+    console.log(a);
+
+
+    //==========================================================================>>
+    // function one()
+    // {
+    //     const  userName = "Uday"
+    //     function two()
+    //     {
+    //         const website = "Youtube"
+    //         console.log(userName);
+    //     }
+       
+    //     //two()
+    // }
+    // one()
+    //=================================================================================>>
+    // if(true)
+    //     {
+    //         const userName = "Uday"
+    //         if(userName === "uday")
+    //             {
+    //                 const website = "youtube"
+    //                 console.log(userName + website);
+    //             }
+    //             console.log(userName);
+    //     }
+
+        //===============================================================================>>
+        if (true) {
+            
+            const username = "hitesh"
+            
+             if (username === "hitesh") {
+           
+               const website = " youtube"
+            
+                console.log(username + website);
+           }
+           
+                  //console.log(website);
+           }
+                 //console.log(username);
+            //======================================================================================>>
+
+
+                 function addone (num) {
+                  return num + 1
+                }
+                    addTwo (5)
+                    
+                    const addTwo = function(num) {
+                    
+                    return num + 2
+                   }
